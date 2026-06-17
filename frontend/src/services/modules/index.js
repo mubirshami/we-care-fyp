5bbe8e9c5975ce594272364664a43e7937344032
mubirshami <mubirshami2000@gmail.com>
2026-06-18 01:46:21 +0500

    refactor: remove unused pages and services

--- frontend/src/services/modules/index.js
+++
@@ -1,8 +0,0 @@
-import auth from './auth';
-import users from './users';
-import reviews from './reviews';
-import videos from './videos';
-import books from './books';
-import journals from './journals';
-
-export { auth, users, reviews, videos, books, journals };
