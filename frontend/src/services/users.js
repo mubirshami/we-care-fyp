af4848e51c1e6b70c543020b30df537027e628a1
mubirshami <mubirshami2000@gmail.com>
2026-06-18 01:49:23 +0500

    feat: add Resources component with VideosTab and BooksTab for wellness resources

---
+++ frontend/src/services/users.js
@@ -0,0 +1,14 @@
+import api from './api';
+
+const getAll = (token) =>
+  api.get('/user/get', token ? { headers: { Authorization: `Bearer ${token}` } } : {});
+const deleteUser = (id, token) =>
+  api.delete(`/user/${id}`, token ? { headers: { Authorization: `Bearer ${token}` } } : {});
+const getById = (id, token) =>
+  api.get(`/user/${id}`, token ? { headers: { Authorization: `Bearer ${token}` } } : {});
+const update = (id, data, token) =>
+  api.put(`/user/${id}`, data, token ? { headers: { Authorization: `Bearer ${token}` } } : {});
+const getId = (token) =>
+  api.get('/user/getid', token ? { headers: { Authorization: `Bearer ${token}` } } : {});
+
+export default { getAll, deleteUser, getById, update, getId };
---
+++ frontend/src/services/videos.js
@@ -0,0 +1,25 @@
+import api from './api';
+
+const getAll = (token) =>
+  api.get('/videos/getall', token ? { headers: { Authorization: `Bearer ${token}` } } : {});
+const postMeditationTime = (payload, token) =>
+  api.post(
+    '/meditationtime/post',
+    payload,
+    token ? { headers: { Authorization: `Bearer ${token}` } } : {}
+  );
+const getCategories = (token) =>
+  api.get('/videos/getcategory', token ? { headers: { Authorization: `Bearer ${token}` } } : {});
+const postVideo = (payload, token) =>
+  api.post(
+    '/videos/videourl',
+    payload,
+    token ? { headers: { Authorization: `Bearer ${token}` } } : {}
+  );
+const remove = (id, token) =>
+  api.delete(
+    `/videos/delete/${id}`,
+    token ? { headers: { Authorization: `Bearer ${token}` } } : {}
+  );
+
+export default { getAll, postMeditationTime, getCategories, postVideo, remove };
